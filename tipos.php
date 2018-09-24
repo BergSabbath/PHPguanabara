d5b216957c2041ce5fd7f0dcc902860bb5b7e5df
BergSabbath <bergit7@gmail.com>
2018-09-24 10:04:49 -0300

    adicionando

---
+++ tipos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8"/>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <title></title>
+</head>
+<body>
+<div>
+    <?php
+      $nome = "Maria";
+      $idade = 18;
+      $media = 4.5;
+      $penal = 1;
+    echo "$nome tem $idade anos! e $media de média escolar.
+    ainda resolvendo conflitosaheuhaeuhuheueahea
+    azsxdcfvgbhjzsxdcfvgbhzsxdcfvgbhj
+    111111111111111111111111111111"
+
+    ?>
+</div>
+</body>
+</html>
